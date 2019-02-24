6b3d79dd665aee0cfd7a38c6a1630eae6f9d62d1
danielsonio <danny@dannywalden.com>
2019-02-24 18:37:19 -0500

    added start script in package.json

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ router.get('/',function(req,res){
 
 
 app.use('/', router);
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
