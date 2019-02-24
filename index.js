ea8d52ab635ea7435636a93c6ffbe6d4c78800e5
danielsonio <danny@dannywalden.com>
2019-02-24 18:30:32 -0500

    updated port for heroku

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const router = express.Router()
-const port = 3000
 
 
 const myLogger = function (req, res, next) {
@@ -25,4 +24,6 @@ router.get('/',function(req,res){
 
 
 app.use('/', router);
-app.listen(process.env.port || 3000)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
